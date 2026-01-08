e772ea080c062aa96992a3b714d9628bb8a6b382
AnikyMX <kamalulgaming21@gmail.com>
2026-01-08 13:32:22 +0800

    [GL4ES 3]: Fix Strings GPUs

--- src/gl/init.c
+++ src/gl/init.c
@@ -399,7 +399,7 @@ void initialize_gl4es() {
         snprintf(globals4es.version, 49, "%s gl4es wrapper %d.%d.%d", env_version, MAJOR, MINOR, REVISION);
         SHUT_LOGD("Targeting OpenGL %s\n", env_version);
     } else {
-        snprintf(globals4es.version, 49, "%d.%d gl4es wrapper %d.%d.%d", globals4es.gl/10, globals4es.gl%10, MAJOR, MINOR, REVISION);
+        snprintf(globals4es.version, 49, "%d.%d GL4ES 3 Wrapper (%s)", globals4es.gl/10, globals4es.gl%10, hardext.renderer);
         SHUT_LOGD("Targeting OpenGL %d.%d\n", globals4es.gl/10, globals4es.gl%10);
     }
 
