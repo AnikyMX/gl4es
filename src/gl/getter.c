329cd58ee2fd4d7038b050ab2d9837c4758bab88
AnikyMX <kamalulgaming21@gmail.com>
2026-01-08 14:16:06 +0800

    [GL4ES 3]: Creating GPU Detection

--- src/gl/getter.c
+++ src/gl/getter.c
@@ -252,10 +252,11 @@ const GLubyte* APIENTRY_GL4ES gl4es_glGetString(GLenum name) {
             return glstate->extensions;
 		case GL_VENDOR:
 			return (GLubyte *)"ptitSeb & AnikyMX";
-		case GL_RENDERER:
+		case GL_RENDERER: {
             static char renderer_buff[128];
             snprintf(renderer_buff, 127, "(%s)", hardext.renderer);
 			return (GLubyte *)renderer_buff;
+		}
 		case GL_SHADING_LANGUAGE_VERSION:
             if(globals4es.gl==21)
             return (GLubyte *)"1.20 via gl4es";
